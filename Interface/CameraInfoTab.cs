69b3552351b1dc783aa224c174b8a0dfd371d6cb
AClouDitation <25907312+AClouDitation@users.noreply.github.com>
2022-09-05 20:03:52 -0700

    fix something stupid

--- Interface/CameraInfoTab.cs
+++ Interface/CameraInfoTab.cs
@@ -25,19 +25,19 @@ namespace CottonCollector.Interface
             ImGui.SameLine();
             ImGui.Text($"Player X: {playerPos.X:#.00}");
             ImGui.SameLine();
-            ImGui.Text($"delta X: {playerPos.X - CameraHelpers.collection->WorldCamera->X}:#.00");
+            ImGui.Text($"delta X: {playerPos.X - CameraHelpers.collection->WorldCamera->X:#.00}");
             // For cameras, Z is actually Y
             ImGui.Text($"Camera Y: {CameraHelpers.collection->WorldCamera->Z:#.00}");
             ImGui.SameLine();
             ImGui.Text($"Player Y: {playerPos.Y:#.00}");
             ImGui.SameLine();
-            ImGui.Text($"delta Y: {playerPos.Y - CameraHelpers.collection->WorldCamera->Z}:#.00");
+            ImGui.Text($"delta Y: {playerPos.Y - CameraHelpers.collection->WorldCamera->Z:#.00}");
             // Ditto
             ImGui.Text($"Camera Z: {CameraHelpers.collection->WorldCamera->Y:#.00}");
             ImGui.SameLine();
             ImGui.Text($"Player Z: {playerPos.Z:#.00}");
             ImGui.SameLine();
-            ImGui.Text($"delta Z: {playerPos.Z - CameraHelpers.collection->WorldCamera->Y}:#.00");
+            ImGui.Text($"delta Z: {playerPos.Z - CameraHelpers.collection->WorldCamera->Y:#.00}");
         }
     }
 }
